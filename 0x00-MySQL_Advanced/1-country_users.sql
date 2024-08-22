9cafbbf35d1a26337f60376d43a2d6e2e165faa4
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-22 06:19:47 +0000

    task0

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
-    country VARCHAR(255) enum('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    country enum('US', 'CO', 'TN') NOT NULL
 );
