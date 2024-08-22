6bde5d677047e3bb03185169a10a7ccd846febf1
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-22 20:49:52 +0000

    task3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- lists all bands with Glam rock
 
-SELECT band_name, COALESCE(split, 2022) - formed AS l
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan 
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY l DESC;
+ORDER BY lifespan DESC;
