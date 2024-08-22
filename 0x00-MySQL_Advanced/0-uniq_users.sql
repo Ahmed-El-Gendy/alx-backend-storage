37fe1f739907434d6bd52b38c595f7808c573917
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-22 06:12:25 +0000

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,4 @@
 --  Create a table named users with the following columns:
---  id: an integer that is the primary key of the table and is autoincremented
---  name: a string that is unique and cannot be null
---  email: a string that can be null
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
