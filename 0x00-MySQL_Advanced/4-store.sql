a72d7e58b14e0a774561d2efc209d60b17aff974
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-22 20:55:46 +0000

    task4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,7 +3,7 @@
 CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.quantity
-  WHERE name = NEW.item_name;
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE name = NEW.item_name;
 END;
