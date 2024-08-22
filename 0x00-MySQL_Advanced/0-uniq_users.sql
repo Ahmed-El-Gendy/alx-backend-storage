4f8fa9cd5b20d3076b80fe80fffadb55afd6990c
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-22 06:14:21 +0000

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,12 @@
 --  Create a table named users with the following columns:
+-- id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+-- name VARCHAR(255) NOT NULL UNIQUE
+-- email VARCHAR(255)
+-- The name column should be unique.
+-- The table should only be created if it does not already exist.
 
 CREATE TABLE IF NOT EXISTS users (
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL UNIQUE,
-        email VARCHAR(255)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255)
 );
