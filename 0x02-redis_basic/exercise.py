68c162fe031dbe8e6c00c4cab32de80e84ed2bc2
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-28 23:19:07 +0000

    task0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Redis module
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ Cache class
+    """
+
+    def __init__(self):
+        """ Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Generate a random key, store the input data in Redis using the key
+        and return the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
